36641f54ca9503e12353dcd03c05961c919a8b02
emmeiwhite <emmeiwhite@gmail.com>
2025-03-11 23:04:44 +0530

    rtk-1.1 setup out store

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
 
-import './index.css';
-import App from './App';
+import './index.css'
+import App from './App'
+import { Provider } from 'react-redux'
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+const container = document.getElementById('root')
+const root = createRoot(container)
+import { store } from './store'
 
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
-);
+  </Provider>
+)
