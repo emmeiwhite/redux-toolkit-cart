3ad7604cfc19075131b48a6bb53dd5c085632564
emmeiwhite <emmeiwhite@gmail.com>
2025-03-11 23:27:05 +0530

    rtk-1.2 setup cart in store

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  cartItems: [],
+  totalItems: 0,
+  total: 0,
+  isLoading: false
+}
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState
+})
+
+console.log(cartSlice) // This cartSlice object contains bunch of properties and one property is reducer which we will export
+
+export default cartSlice.reducer
