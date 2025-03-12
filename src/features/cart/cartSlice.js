5e84df86637eeb5f80779ad5f0320c425e296240
emmeiwhite <emmeiwhite@gmail.com>
2025-03-12 15:45:47 +0530

    rtk-1.9 increase / decrease functionality

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -18,11 +18,26 @@ const cartSlice = createSlice({
       //   console.log(payload)
       const itemId = action.payload
       state.cartItems = state.cartItems.filter(item => item.id !== itemId)
+    },
+    increase: (state, { payload }) => {
+      //   console.log(payload) // payload is object in this case
+      const cartItem = state.cartItems.find(item => item.id === payload.id)
+      cartItem.amount = cartItem.amount + 1
+    },
+    increase: (state, { payload }) => {
+      const cartItem = state.cartItems.find(item => item.id === payload.id)
+      cartItem.amount = cartItem.amount + 1
+      state.totalItems = state.totalItems + 1
+    },
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find(item => item.id === payload.id)
+      cartItem.amount = cartItem.amount - 1
+      state.totalItems = state.totalItems - 1
     }
   }
 })
 
 console.log(cartSlice) // This cartSlice object contains bunch of properties and one property is reducer which we will export
-export const { clearCart, removeItem } = cartSlice.actions
+export const { clearCart, removeItem, increase } = cartSlice.actions
 
 export default cartSlice.reducer
